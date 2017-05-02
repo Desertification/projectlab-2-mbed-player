b1f28a787756f073b745a380cabb983b4e32ccea
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-05-03 00:41:21 +0200

    uart thread test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 
 #include "mbed.h"
+#include "rtos.h"
+
+Serial out(p9, p10); //p9
+Serial in(p13, p14); //p14
+
+
+void receive_uart() {
+    while (true){
+        if(in.readable()){
+            char s[32];
+            printf(in.gets(s, 32));
+        }else{
+            Thread::yield();
+        }
+    }
+}
 
 int main() {
-    printf("%s", "hello, term");
+    printf("%s\n", "hello, term");
+
+    while (true){
+        out.printf("hello, term\n");
+        wait(1);
+    }
+
 }
